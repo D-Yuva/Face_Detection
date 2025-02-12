e16dfce21d317cacae02129fb27008aeee7cd088
D-Yuva <dyuva2005@gmail.com>
2025-02-12 16:38:08 +0530

    Deleted test

--- test.py
+++
@@ -1,7 +0,0 @@
-import face_recognition
-
-# Load a sample picture and learn how to recognize it.
-image = face_recognition.load_image_file("path_to_a_sample_image.jpg")
-face_encoding = face_recognition.face_encodings(image)[0]
-
-print("Face encoding completed successfully.")
